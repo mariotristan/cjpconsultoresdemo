67e34df6452d688ec9d0b61e503a8b92f9ea3bce
Mario Tristan <mario.tristanmartine@slalom.com>
2025-07-13 17:25:11 -0600

    first draft

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cjpconsultoresdemo/' // Cambia esto si tu repo tiene otro nombre
+  base: 'https://mariotristan.github.io/cjpconsultoresdemo' // Cambia esto si tu repo tiene otro nombre
 })
